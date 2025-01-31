c2acaccd64a63962b3b5041776ba1075dcc1c877
meruertm0 <m_mukhtarkyzy@kbtu.kz>
2025-01-31 22:21:05 +0500

    Change Dictionary Items

---
+++ changeitems.py
@@ -0,0 +1,14 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+
+#The update() method will update the dictionary with the items from the given argument.
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
